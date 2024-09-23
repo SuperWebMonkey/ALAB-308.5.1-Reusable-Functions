0722d310dfbd0250ebcb41669595f123bc30f43b
SuperWebMonkey <vikingsattack@outlook.com>
2024-09-23 13:57:20 -0700

    longest str finished

--- script.js
+++ script.js
@@ -18,14 +18,19 @@ function avg(ary) {
 }
 
 // get longest str in str ary
-function longest(strAry) {
+function longestString(strAry) {
   let len = strAry.length;
   let max = 0;
+  let tracker = 0;
   for (let i = 0; i < len; i++) {
     if (strAry[i].length > max) {
+      //   console.log(max);
       max = strAry[i].length;
+      tracker = i;
     }
   }
+  //   console.log(strAry[tracker]);
+  return strAry[tracker];
 }
 
 // recursions
@@ -39,4 +44,9 @@ function rec(n, i) {
   }
 }
 
-rec(10, 1);
+// used for longest str ary
+const strAry = ["a", "bbb", "abcd", "ccc"];
+const longestStr = longestString(strAry);
+console.log(longestStr);
+
+// rec(10, 1); // for the recursion method
